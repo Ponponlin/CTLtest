d754f28a7d12eb2a4984a5b344b6bf743752491a
Ponpon <ponpon225487@gmail.com>
2021-03-10 17:50:51 +0800

    刪除測試githup

--- MemberManage/MemberAddTest.cs
+++ MemberManage/MemberAddTest.cs
@@ -138,8 +138,6 @@ namespace CTLtest.MemberManage
             warringstring =  driver.FindElement(By.CssSelector("#MemberManage > div:nth-child(7) > div > div.el-dialog__body > form > div.el-form-item.el-form-item--feedback.is-error.is-required.el-form-item--small > div > div.el-form-item__error"))
                                                  .GetAttribute("innerText");
 
-            //test123
-            //test456
             
             switch (option) {
                 case "未輸入":
